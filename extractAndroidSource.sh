8a8f2a31b20a1d70213449d0bf6e0b151b735bb4
manningr <manningr@7924a010-0c6b-448a-821b-8eebd99e1a47>
2010-07-03 21:03:27 +0000

    Added support for building android SDK jar with implementation code instead of stubs

--- extractAndroidSource.sh
+++ extractAndroidSource.sh
@@ -16,7 +16,7 @@
 #    android-4 : 1.6_r2
 #    android-6 : 2.0.1_r1
 #    android-7 : 2.1_r1
-export pomVersion=2.2
+export pomVersion=1.6_r2
 
 # Where was the android repo created and sync'd
 export droidFolder=/home/manningr/mydroid-$pomVersion
@@ -37,24 +37,54 @@ export branchtag=android-$branchVersion
 
 export projectsFolder=`pwd`/target-$pomVersion
 export androidProjectFolder=$projectsFolder/android-$branchtag
+export androidImplProjectFolder=$projectsFolder/android-impl-$branchtag
 export junitProjectFolder=$projectsFolder/android-junit-$branchtag
 export khronosProjectFolder=$projectsFolder/khronos-$branchtag
 export androidTestProjectFolder=$projectsFolder/android-test-$branchtag
+export androidTestImplProjectFolder=$projectsFolder/android-test-impl-$branchtag
 export droidOutFolder=$droidFolder/out
 export androidSrcFolder=$androidProjectFolder/src/main/java
 export androidResourcesFolder=$androidProjectFolder/src/main/resources
+export androidImplSrcFolder=$androidImplProjectFolder/src/main/java
+export androidImplResourcesFolder=$androidImplProjectFolder/src/main/resources
 export junitSrcFolder=$junitProjectFolder/src/main/java
 export junitResourcesFolder=$junitProjectFolder/src/main/resources
 export khronosSrcFolder=$khronosProjectFolder/src/main/java
 export androidTestSrcFolder=$androidTestProjectFolder/src/main/java
+export androidTestImplSrcFolder=$androidTestImplProjectFolder/src/main/java
 
 export droidSrcFolder=$droidFolder/out/target/common/obj/JAVA_LIBRARIES/android_stubs_current_intermediates/src
 export androidPomfile=`pwd`/android-pom.xml
 export junitPomFile=`pwd`/junit-pom.xml
 export khronosPomFile=`pwd`/khronos-pom.xml
 export androidTestPomFile=`pwd`/android-test-pom.xml
+export androidImplPomfile=`pwd`/android-impl-pom.xml
 
 
+function copyResources {
+	resourcesFolder=$1
+	echo "Copying resources files to android"
+	cp -r $droidOutFolder/target/common/obj/JAVA_LIBRARIES/android_stubs_current_intermediates/classes/res $resourcesFolder
+	rm -rf $resourcesFolder/res/raw-ar
+	rm -rf $resourcesFolder/res/raw-da
+	rm -rf $resourcesFolder/res/raw-fi
+	rm -rf $resourcesFolder/res/raw-hu
+	rm -rf $resourcesFolder/res/raw-iw
+	rm -rf $resourcesFolder/res/raw-pt-BR
+	rm -rf $resourcesFolder/res/raw-th
+	rm -rf $resourcesFolder/res/raw-tr
+
+	cp -r $droidOutFolder/target/common/obj/JAVA_LIBRARIES/android_stubs_current_intermediates/classes/assets $resourcesFolder
+	cp $droidOutFolder/target/common/obj/JAVA_LIBRARIES/android_stubs_current_intermediates/classes/resources.arsc $resourcesFolder
+
+	platform=`ls $droidOutFolder/host/linux-x86/sdk/android-sdk_eng."$USERNAME"_linux-x86/platforms`
+	cp -r $droidOutFolder/host/linux-x86/sdk/android-sdk_eng."$USERNAME"_linux-x86/platforms/"$platform"/data/res/drawable-hdpi $resourcesFolder/res
+	cp -r $droidOutFolder/host/linux-x86/sdk/android-sdk_eng."$USERNAME"_linux-x86/platforms/"$platform"/data/res/drawable-land-hdpi $resourcesFolder/res
+	cp $droidOutFolder/target/common/obj/JAVA_LIBRARIES/android_stubs_current_intermediates/classes/AndroidManifest.xml $resourcesFolder
+}
+
+
+# BEGIN
 
 echo "Removing $projectsFolder"
 rm -rf $projectsFolder
@@ -63,9 +93,16 @@ echo "Setting up Android Maven project folders"
 mkdir -p $androidSrcFolder
 mkdir -p $androidResourcesFolder
 
+echo "Setting up Android Implementation Maven project folders"
+mkdir -p $androidImplSrcFolder
+mkdir -p $androidImplResourcesFolder
+
 
 echo "Setting up Android Test Maven project folders"
 mkdir -p $androidTestSrcFolder/android
+
+echo "Setting up Android Test Implementation Maven project folders"
+mkdir -p $androidTestImplSrcFolder/android
 
 
 echo "Setting up Android-JUnit Maven project folders"
@@ -108,36 +145,67 @@ rm -rf $androidSrcFolder/javax/xml
 
 cp -r $droidSrcFolder/javax/microedition $khronosSrcFolder/javax
 
-#cp -r $droidSrcFolder/javax/crypto $cryptoSrcFolder/javax
-#cp -r $droidSrcFolder/org $androidSrcFolder
-#rm -rf $androidSrcFolder/org/apache/commons/logging
-#rm -rf $androidSrcFolder/org/apache/http
-#rm -rf $androidSrcFolder/org/w3c
-#rm -rf $androidSrcFolder/org/xml
-#rm -rf $androidSrcFolder/org/xmlpull
+# Android Impl (For now, just trying to get a build that compiles - there are many 3rd-party sources being included)
+cp -r $droidFolder/frameworks/base/core/java/* $androidImplSrcFolder/
+rm -rf $androidImplSrcFolder/com/android/os
+cp -r $droidFolder/frameworks/base/core/config/sdk/* $androidImplSrcFolder/
+cp -r $droidFolder/frameworks/base/graphics/java/* $androidImplSrcFolder/
+cp -r $droidFolder/frameworks/base/keystore/java/* $androidImplSrcFolder/
+cp -r $droidFolder/frameworks/base/location/java/* $androidImplSrcFolder/
+cp -r $droidFolder/frameworks/base/media/java/* $androidImplSrcFolder/
+cp -r $droidFolder/frameworks/base/sax/java/* $androidImplSrcFolder/
+cp -r $droidFolder/frameworks/base/telephony/java/* $androidImplSrcFolder/
+cp -r $droidFolder/frameworks/base/wifi/java/* $androidImplSrcFolder/
+cp -r $droidFolder/frameworks/base/location/java/* $androidImplSrcFolder/
 
-echo "Copying resources files"
-cp -r $droidOutFolder/target/common/obj/JAVA_LIBRARIES/android_stubs_current_intermediates/classes/res $androidResourcesFolder
-rm -rf $androidResourcesFolder/res/raw-ar
-rm -rf $androidResourcesFolder/res/raw-da
-rm -rf $androidResourcesFolder/res/raw-fi
-rm -rf $androidResourcesFolder/res/raw-hu
-rm -rf $androidResourcesFolder/res/raw-iw
-rm -rf $androidResourcesFolder/res/raw-pt-BR
-rm -rf $androidResourcesFolder/res/raw-th
-rm -rf $androidResourcesFolder/res/raw-tr
+cp -r $droidOutFolder/target/common/obj/JAVA_LIBRARIES/framework_intermediates/src/core/java/* $androidImplSrcFolder/
+cp -r $droidFolder/dalvik/libcore/dalvik/src/main/java/org $androidImplSrcFolder/
+cp -r $droidFolder/dalvik/libcore/dalvik/src/main/java/dalvik $androidImplSrcFolder/
+cp -r $droidFolder/dalvik/libcore/x-net/src/main/java/org $androidImplSrcFolder/
+cp -r $droidFolder/dalvik/libcore/xml/src/main/java/org/apache $androidImplSrcFolder/org/
+cp -r $droidFolder/dalvik/libcore/xml/src/main/java/org/kxml2 $androidImplSrcFolder/org/
+# Google's org.w3c.dom.Node doesn't have an abstract getUserData; Java 1.5 ships a org.w3c.dom.Node that does.  
+cp -r $droidFolder/dalvik/libcore/xml/src/main/java/org/w3c $androidImplSrcFolder/org/
+cp -r $droidFolder/dalvik/libcore/luni/src/main/java/org $androidImplSrcFolder/
+# Patched version of Java API classes 
+mkdir -p $androidImplSrcFolder/java
+cp -r $droidFolder/dalvik/libcore/luni/src/main/java/java/net $androidImplSrcFolder/java
 
-cp -r $droidOutFolder/target/common/obj/JAVA_LIBRARIES/android_stubs_current_intermediates/classes/assets $androidResourcesFolder
-cp $droidOutFolder/target/common/obj/JAVA_LIBRARIES/android_stubs_current_intermediates/classes/resources.arsc $androidResourcesFolder
+mkdir -p $androidImplSrcFolder/com/android/internal
+cp -r $droidFolder/out/target/common/R/com/android/internal/R.java $androidImplSrcFolder/com/android/internal
 
-platform=`ls $droidOutFolder/host/linux-x86/sdk/android-sdk_eng."$USERNAME"_linux-x86/platforms`
-cp -r $droidOutFolder/host/linux-x86/sdk/android-sdk_eng."$USERNAME"_linux-x86/platforms/"$platform"/data/res/drawable-hdpi $androidResourcesFolder/res
-cp -r $droidOutFolder/host/linux-x86/sdk/android-sdk_eng."$USERNAME"_linux-x86/platforms/"$platform"/data/res/drawable-land-hdpi $androidResourcesFolder/res
-cp $droidOutFolder/target/common/obj/JAVA_LIBRARIES/android_stubs_current_intermediates/classes/AndroidManifest.xml $androidResourcesFolder
+cp -r $droidFolder/out/target/common/obj/JAVA_LIBRARIES/framework_intermediates/src/telephony/java/com $androidImplSrcFolder/
+cp -r $droidFolder/out/target/common/obj/JAVA_LIBRARIES/framework_intermediates/src/location/java/* $androidImplSrcFolder/
+cp -r $droidFolder/out/target/common/obj/JAVA_LIBRARIES/framework_intermediates/src/media/java/* $androidImplSrcFolder/
+cp -r $droidFolder/out/target/common/obj/JAVA_LIBRARIES/framework_intermediates/src/wifi/java/* $androidImplSrcFolder/
+
+cp -r $droidFolder/out/target/common/obj/APPS/framework-res_intermediates/src/android $androidImplSrcFolder/
+
+# This is org.apache.harmony and org.bouncycastle which appear to be patched versions of the same
+cp -r $droidFolder/dalvik/libcore/security/src/main/java/org $androidImplSrcFolder/
+
+# This is org.apache.http which is a patched version of httpcomponents 4.0-beta1
+cp -r $droidFolder/external/apache-http/src/org $androidImplSrcFolder/
+
+cp -r $droidFolder/external/gdata/src/* $androidImplSrcFolder/
+
+find $androidImplSrcFolder -name "*.aidl" | xargs rm 
+find $androidImplSrcFolder -name "*.P" | xargs rm 
+
+echo "Copying resources files to android"
+copyResources $androidResourcesFolder
+copyResources $androidImplResourcesFolder
 
 echo "Copying in pom files ($androidPomfile and $junitPomFile)"
 cp $androidPomfile $androidProjectFolder/pom.xml
 perl -pi -e "s/\@VERSION\@/$pomVersion/" $androidProjectFolder/pom.xml
+
+cp $androidTestPomFile $androidTestProjectFolder/pom.xml
+perl -pi -e "s/\@VERSION\@/$pomVersion/" $androidTestProjectFolder/pom.xml
+
+cp $androidImplPomfile $androidImplProjectFolder/pom.xml
+perl -pi -e "s/\@VERSION\@/$pomVersion/" $androidImplProjectFolder/pom.xml
+
 
 cp $junitPomFile $junitProjectFolder/pom.xml
 perl -pi -e "s/\@VERSION\@/$pomVersion/" $junitProjectFolder/pom.xml
@@ -145,8 +213,6 @@ perl -pi -e "s/\@VERSION\@/$pomVersion/" $junitProjectFolder/pom.xml
 cp $khronosPomFile $khronosProjectFolder/pom.xml
 perl -pi -e "s/\@VERSION\@/$pomVersion/" $khronosProjectFolder/pom.xml
 
-cp $androidTestPomFile $androidTestProjectFolder/pom.xml
-perl -pi -e "s/\@VERSION\@/$pomVersion/" $androidTestProjectFolder/pom.xml
 
 cd $junitProjectFolder
 mvn clean install
@@ -158,4 +224,7 @@ cd $androidProjectFolder
 mvn clean install
 
 cd $androidTestProjectFolder
+mvn clean install
+
+cd $androidImplProjectFolder
 mvn clean install
